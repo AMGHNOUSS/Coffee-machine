58ce15a5448406a6248be93793592b74e019b5ad
Redouane AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-10-03 02:48:23 +0100

    Create File main.py

---
+++ Coffee-machine-OOP/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# The main program.
+if __name__ == "__main__":
+    state = True
+    # while state:
+    #     res = input("What would you like? (espresso/latte/cappuccino):")
+    #     if res == "off":
+    #         state = False
