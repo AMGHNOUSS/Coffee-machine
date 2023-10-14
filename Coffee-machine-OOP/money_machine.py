5c2e56c9259d7000d586677c9665b53b1a7ff8e8
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-10-14 23:29:10 +0100

    Create Class money_machine

--- Coffee-machine-OOP/money_machine.py
+++ Coffee-machine-OOP/money_machine.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
 
+class MoneyMachine:
+    """Class Money machine"""
+    def __init__(self):
+        self.quarters = 0.25
+        self.dimes = 0.1
+        self.nickles = 0.05
+        self.pennies = 0.01
+    
+    def calcul(self, q, d, n, p):
+        """Calcul Money"""
+        result = (self.quarters * float(q)) + (self.dimes * float(d)) + \
+        (self.nickles * float(n)) + (self.pennies * float(p))
+        return result
