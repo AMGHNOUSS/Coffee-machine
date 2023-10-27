ad8f8834d61ecaefc9e88e40fab7e104e932a2af
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2023-10-27 21:30:54 +0100

    Create Function test resources sufficient

--- Coffee-machine-OOP/TestCoffee_marker.py
+++ Coffee-machine-OOP/TestCoffee_marker.py
@@ -1,6 +1,7 @@
 
 import unittest
 from coffee_maker import CoffeeMaker
+from menu import MenuItem
 
 class TestCoffeeMarker(unittest.TestCase):
 
@@ -11,3 +12,8 @@ class TestCoffeeMarker(unittest.TestCase):
     def test_report(self):
         """Test function report"""
         self.assertEqual(self.cm.report(), None)
+    
+    def test_resources_sufficient(self):
+        """Test function resources sufficient"""
+        mi = MenuItem("espresso", 1.5, 50, 0, 18)
+        self.assertEqual(self.cm.resources_sufficient(mi), 1)
